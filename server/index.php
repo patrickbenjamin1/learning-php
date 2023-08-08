2f99d3cb112fd969e5c00de4637a49ec71a03f14
pat <dev@patrickbenjam.in>
2023-08-08 18:29:07 +0100

    move header to components, add footer

--- server/index.php
+++ server/index.php
@@ -3,6 +3,9 @@
 </head>
 
 <body>    
-    <?php require __DIR__.'/header.php' ?>
+    <?php require __DIR__.'/components/header.php' ?>
+    
     <?php require __DIR__.'/router.php' ?>
+
+    <?php require __DIR__.'/components/footer.php' ?>
 </body>
