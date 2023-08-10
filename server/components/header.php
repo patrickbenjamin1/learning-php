f7da3e079967b15c15de09adc64387ec72e1ee51
pat <dev@patrickbenjam.in>
2023-08-10 12:54:15 +0100

    fix header link hrefs not being echod

--- server/components/header.php
+++ server/components/header.php
@@ -6,14 +6,14 @@
         <a 
             class='header-navigation-link' 
             <?php echo ($path===$routes['thing']['route']('hello')) ? "data-current" : "" ?> 
-            href="<?php $routes['thing']['route']('hello') ?>"
+            href="<?php echo $routes['thing']['route']('hello') ?>"
         >
             hello
         </a>
         <a 
             class='header-navigation-link' 
             <?php echo ($path===$routes['thing']['route']('world')) ? "data-current" : "" ?> 
-            href="<?php $routes['thing']['route']('world') ?>"
+            href="<?php echo $routes['thing']['route']('world') ?>"
         >
             world
         </a>
